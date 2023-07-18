435d127eb1a19df9b5eab96d048d29814cce0f95
Bennett Dams <dams.bennett@gmail.com>
2023-07-19 00:42:50 +0200

    Subtle background gradient color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`container mx-auto min-h-screen bg-gradient-to-br from-indigo-950 to-violet-950 p-10 text-indigo-100 lg:p-24 ${rubikFont.className}`}
+        className={`container mx-auto min-h-screen bg-gradient-to-b from-indigo-950 via-indigo-900 to-slate-900 p-10 text-indigo-100 lg:p-24 ${rubikFont.className}`}
       >
         {children}
       </body>
